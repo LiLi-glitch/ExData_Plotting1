49cab5122e77ca780d9784a341bf12f775f41440
LiLi <60715815+LiLi-glitch@users.noreply.github.com>
2020-07-12 22:53:25 +0200

    Made plot1.png and plot1.r

---
+++ plot1.R
@@ -0,0 +1,30 @@
+library(tidyr)
+
+#Download data and put in dataframe 
+Dataset <- download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "HPC.zip")
+unzip(zipfile = "./HPC.zip", exdir = ".")
+
+#load text file in dataframe, skip the first row because that contains the columns and seperate based on ; 
+HPC <- read.table("household_power_consumption.txt", skip = 1, sep = ";")
+
+#name the columns again
+names(HPC)<- c("Date", "Time", "Global_active_power", "Global_reactive_power", 
+               "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+#subset data between 2007-02-01 and 2007-02-02
+HPC$Date <- as.Date(HPC$Date, format = "%d/%m/%Y")
+HPC1 <- subset(HPC, HPC$Date == "2007/2/1" | HPC$Date == "2007/2/2")
+
+#plot1 is a histogram, frequency of Global_active_power. 
+#Change class Global_active_power to character to categorize
+HPC$Global_active_power <- as.character(HPC$Global_active_power)
+hist(as.numeric(as.character(HPC1$Global_active_power)), col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power(kilowatts)")
+
+#save plot1 as png
+png("plot1.png", width = 480, height = 480)
+
+hist(as.numeric(as.character(HPC1$Global_active_power)), col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power(kilowatts)")
+
+dev.off()
